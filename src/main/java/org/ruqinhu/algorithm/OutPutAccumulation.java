0969bc3d6b181618515107468d9a43d7bce40dfa
ruqinhu <542849951@qq.com>
2021-12-02 13:27:47 +0800

    多线程顺序输出

---
+++ src/main/java/org/ruqinhu/algorithm/OutPutAccumulation.java
@@ -0,0 +1,80 @@
+package org.ruqinhu.algorithm;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 3 个线程 累加 1 - 100  结果是 5050
+ */
+public class OutPutAccumulation {
+
+    private static Integer i = 0;
+
+    private static Integer j = 0;
+
+    private static final ReentrantLock lock = new ReentrantLock();
+
+    private static class Runnable1 implements Runnable {
+        @Override
+        public void run() {
+            while (true) {
+                lock.lock();
+                if (i == 5050) return;
+                i = i + j++;
+                System.out.println(i);
+                lock.unlock();
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private static class Runnable2 implements Runnable {
+        @Override
+        public void run() {
+            while (true) {
+                lock.lock();
+                if (i == 5050) return;
+                i = i + j++;
+                System.out.println(i);
+                lock.unlock();
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private static class Runnable3 implements Runnable {
+        @Override
+        public void run() {
+            while (true) {
+                lock.lock();
+                if (i == 5050) return;
+                i = i + j++;
+                System.out.println(i);
+                lock.unlock();
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread1 = new Thread(new Runnable1());
+        thread1.start();
+        Thread thread2 = new Thread(new Runnable2());
+        thread2.start();
+        Thread thread3 = new Thread(new Runnable3());
+        thread3.start();
+        Thread.sleep(200000);
+    }
+
+}
---
+++ src/main/java/org/ruqinhu/algorithm/OutPutAlphabet2.java
@@ -0,0 +1,64 @@
+package org.ruqinhu.algorithm;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class OutPutAlphabet2 {
+
+    private static String[] letters = new String[]{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+
+    private static ReentrantLock lock = new ReentrantLock();
+
+    private static Boolean isVowel(CharSequence c) {
+        return "aeiou".contains(c);
+    }
+
+    private static class Vowel implements Runnable{
+        @Override
+        public void run() {
+            for (int i = 0;i < letters.length; i++) {
+                lock.lock();
+                if (isVowel(letters[i])) {
+                    System.out.println(letters[i]);
+                }
+                lock.unlock();
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private static class Constant implements Runnable {
+        @Override
+        public void run() {
+            for (int i = 0;i < letters.length; i++) {
+                lock.lock();
+                if (!isVowel(letters[i])) {
+                    System.out.println(letters[i]);
+                }
+                lock.unlock();
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread cowel = new Thread(new Vowel());
+        Thread constant = new Thread(new Constant());
+
+        cowel.start();
+        constant.start();
+
+        Thread.sleep(4000000);
+
+    }
+
+}
