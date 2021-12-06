af2d8475364c920a139f97e717f718d277812c75
ruqinhu <542849951@qq.com>
2021-12-06 17:06:17 +0800

    outmemory 测试

--- src/main/java/org/ruqinhu/jvm/OutOfMemoryError.java
+++ src/main/java/org/ruqinhu/jvm/OutOfMemoryError.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 /**
  * -Xms20m -Xmx20m -XX:+HeapDumpOnOutOfMemoryError
+ *  -XX:HeapDumpPath=D:/code/github/practice/heapdump.dump
+ * GC overhead limit exceeded  多次 gc 释放很少空间
  */
 public class OutOfMemoryError {
 
@@ -15,7 +17,7 @@ public class OutOfMemoryError {
 //        while (true) {
 //            list.add(new OOMObject());
 //        }
-
+//
         int i = 0;
         List<String> list = new ArrayList<>();
         while (true) {
